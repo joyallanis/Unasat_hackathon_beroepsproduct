5a23204d453370bf028de451f3a91785a0cd771e
NoTilt222 <108106585+NoTilt222@users.noreply.github.com>
2023-05-05 21:08:47 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,11 @@ public class Main {
         String choice;  // Declare a String variable named choice.
         do {
             System.out.println("Choose an option below then enter: ");  // Print a message to the console.
-            System.out.println("view");  // Print a message to the console.
-            System.out.println("exit");  // Print a message to the console.
-            choice = scanner.next();  // Read the user's input from the console and store it in the choice variable.
+            System.out.println("View");  // Print a message to the console.
+            System.out.println("Exit");  // Print a message to the console.
+            choice = scanner.next().toLowerCase();  // Read the user's input from the console and store it in the choice variable.
             switch (choice) {  // Use a switch statement to determine which code to execute based on the user's choice.
-                case "view" -> view.viewStudents();  // If the user enters "view", call the viewStudents() method of the view object.
+                case "view" -> view.View();  // If the user enters "view", call the viewStudents() method of the view object.
                 case "exit" -> System.out.println("Exiting program...");  // If the user enters "exit", print a message to the console.
                 default -> System.out.println("Invalid choice.");  // If the user enters anything else, print an error message to the console.
             }
