b0294ffad85a747356b2633252d8ce74b8ac3d6c
NoTilt222 <108106585+NoTilt222@users.noreply.github.com>
2023-05-07 12:10:48 -0300

    Fixed bug members appearing per person

--- src/ViewStudents.java
+++ src/ViewStudents.java
@@ -1,22 +1,54 @@
-import java.sql.*;   // Importing everything(*) is bad practice which causes bloatware but you should onlu import the necessary classes from the package. In this case it's java.sql
-import java.util.Scanner;  // Import the Scanner class from the java.util package. This will be used for input
+import java.sql.*; // Import necessary libraries for database connectivity
+import java.util.HashMap; // Import HashMap class for team tracking
+import java.util.Map; // Import Map interface for team tracking
+import java.util.Objects; // Import Objects class for null checking
+import java.util.Scanner; // Import Scanner class for user input
 
 public class ViewStudents {
-    public void viewStudents() {
-        try (Connection conn = DatabaseConnection.getConnection()) {  // Try to open a new database connection using the getConnection() method from the DatabaseConnection class.
-            String sql = "SELECT * FROM student";  // Create a String variable named sql that contains a SQL SELECT statement to retrieve all data from the student table.
-            PreparedStatement stmt = conn.prepareStatement(sql);  // Prepare a PreparedStatement object with the SQL statement.
-            ResultSet rs = stmt.executeQuery();  // Execute the SQL statement and store the results in a ResultSet object.
-            while (rs.next()) {  // Loop through each row in the ResultSet.
-                int id = rs.getInt("student_id");  // Get the value of the "student_id" column for the current row and store it in an int variable named id.
-                String first = rs.getString("first_name");  // Get the value of the "first_name" column for the current row and store it in a String variable named first.
-                String last = rs.getString("last_name");  // Get the value of the "last_name" column for the current row and store it in a String variable named last.
-                System.out.println("ID: " + id + ", First Name: " + first + ", Last Name: " + last);  // Print out the values of the id, first, and last variables for the current row.
+    public void View() {
+        Scanner scanner = new Scanner(System.in); // Create a scanner object to read user input
+        String choice; // Declare a string variable to hold the user's choice
+        do {
+            System.out.println("Choose an option below then enter: "); // Print instructions for user
+            System.out.println("View Teams"); // Print option to view teams
+            System.out.println("Return"); // Print option to return to previous menu
+            choice = scanner.nextLine().toLowerCase(); // Read user input and convert it to lowercase
+            switch (choice) { // Check the value of the user's input
+                case "view teams" -> ViewTeams(); // If the user wants to view teams, call the ViewTeams method
+                case "return" -> System.out.println("Returning to menu"); // If the user wants to return, print a message
+                default -> System.out.println("Invalid choice."); // If the user enters something else, print an error message
             }
-            rs.close();  // Close the ResultSet object to free up resources.
-            stmt.close();  // Close the PreparedStatement object to free up resources.
-        } catch (SQLException ex) {  // Catch any SQLException that may occur.
-            System.out.println("Error: " + ex.getMessage());  // Print out the error message associated with the SQLException.
+        } while (!Objects.equals(choice, "return")); // Continue looping until the user chooses to return
+    }
+
+    public void ViewTeams() {
+        try (Connection conn = DatabaseConnection.getConnection()) { // Get a connection to the database
+            String sql = "SELECT team.team_id, team.team_naam, student.student_id, student.naam, student.achter_naam, contact_gegevens.unasat_emailadres\n" +
+                    "                    FROM team\n" +
+                    "                    LEFT JOIN student ON team.team_id = student.team_id\n" +
+                    "                    LEFT JOIN contact_gegevens ON student.contact_id = contact_gegevens.contact_id;";
+            PreparedStatement stmt = conn.prepareStatement(sql); // Prepare a SQL statement
+            ResultSet rs = stmt.executeQuery(); // Execute the statement and store the results
+            Map<Integer, String> teamMap = new HashMap<>(); // create a HashMap to keep track of teams
+            while (rs.next()) { // Iterate over the rows of the result set
+                int teamid = rs.getInt("team_id"); // Get the team ID from the current row
+                String teamname = rs.getString("team_naam"); // Get the team name from the current row
+                int studentid = rs.getInt("student_id"); // Get the student ID from the current row
+                String first = rs.getString("naam"); // Get the first name of the student from the current row
+                String last = rs.getString("achter_naam"); // Get the last name of the student from the current row
+                String email = rs.getString("unasat_emailadres"); // Get the email of the student from the current row
+                if (!teamMap.containsKey(teamid)) { // Check if team has already been printed by checking if its ID is in the HashMap
+                    System.out.println("\033[34mTeam:\033[0m"); // Print the word "Team:" in blue color using an escape code
+                    System.out.println("\033[34mID:\033[0m " + teamid + ", \033[34mName:\033[0m " + teamname ); // Print the team ID and name in blue color using an escape code
+                    System.out.println("\033[34mMembers:\033[0m"); // Print the word "Members:" in blue color using an escape code
+                    teamMap.put(teamid, teamname); // Add the team to the HashMap
+                }
+                System.out.println("\033[34mID:\033[0m " + studentid + ", \033[34mName:\033[0m " + first + " " + last + ", \033[34mE-mailadres:\033[0m " + email ); // Print the student ID, name, and email in blue color using an escape code
+            }
+            rs.close(); // Close the result set
+            stmt.close(); // Close the statement
+        } catch (SQLException ex) { // Catch any SQL exceptions that may occur
+            System.out.println("Error: " + ex.getMessage()); // Print the error message
         }
     }
 }
